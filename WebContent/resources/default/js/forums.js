138243320c5b1e0f8d5b9ae19812466225f05cf9
med <mederic.munier@gmail.com>
2013-05-10 11:52:57 +0800

    première session de rattrapage i18n

--- WebContent/resources/default/js/forums.js
+++ WebContent/resources/default/js/forums.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {	
-	$('#mainSection div.message').click(function(){
+	$('#mainSection div.message').dblclick(function(){
     	var currentId = $(this).attr('id').match(/^msg([0-9]+)$/);
     	var messageId = currentId[1];
     	
     	//TODO :
     	// - click IN, masquage du message et affichage de la textArea (préparée dans la page JSF mais masquée via CSS)
     	// - clock OUT, réaffichage du contenu d'origine.
-    	$(this).html("<textarea>Bip bip !</textarea>");
+    	$(this).child( "form" ).show(); 
+    	html("<textarea>Bip bip !</textarea>");
+
 
     	return false;
     });
