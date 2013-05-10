924dd635bf43715d4e2eb94607210eb7b7994dd5
med <mederic.munier@gmail.com>
2013-05-10 22:37:56 +0800

    amélioration UI de l'édition d'une réponse

--- WebContent/resources/default/js/forums.js
+++ WebContent/resources/default/js/forums.js
@@ -7,4 +7,9 @@ $(document).ready(function() {
     	return false;
     });//end of $('#mainSection div.message').click()
     
+	$("#mainSection div.message form input.cancelEdit").click(function(){
+        	$(this).parent( "form" ).hide(); 
+        	$(this).parent( "form" ).siblings("p").show(); 
+    	return false;
+    });//end of $('#mainSection div.message').click()
 });//end of $(document).ready(fn)
