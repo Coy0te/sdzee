46b55a990398ee7047c2686890d0603150b5998c
med <mederic.munier@gmail.com>
2013-05-11 14:23:03 +0800

    essai de correction du bug sur l'impossibilité d'annuler une édition

--- WebContent/resources/default/js/forums.js
+++ WebContent/resources/default/js/forums.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {	
-	$('#mainSection a.editAction').click(function(){
+	$('#mainSection a.editAction').click( function(){
 		var objet = $(this).closest( "div" ).find( "form" );
     	if(objet.length){
     		objet.find( "p" ).hide(); 
@@ -8,7 +8,7 @@ $(document).ready(function() {
     	return false;
     });//end of $('#mainSection div.message').click()
     
-	$("#mainSection div.message form input.cancelEdit").click(function(){
+	$("#mainSection div.message form input.cancelEdit").on( 'click', function(){ // le on() devrait pas gérer le live/ajax re-render ?
         	$(this).parent( "div" ).hide(); 
         	$(this).parent( "div" ).siblings("p").show(); 
     	return false;
