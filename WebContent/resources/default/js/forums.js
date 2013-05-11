7f8ac199ae5be35f4dba8b40fdb0293a0414d9b5
med <mederic.munier@gmail.com>
2013-05-11 14:44:39 +0800

    correction du bug sur l'impossibilité d'annuler une édition

--- WebContent/resources/default/js/forums.js
+++ WebContent/resources/default/js/forums.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {	
-	$('#mainSection a.editAction').click( function(){
+$(document).ready(function() {
+  initAll();
+});//end of $(document).ready(fn)
+
+function initAll(){	
+	$('#mainSection a.editAction').click( function(e){
+		e.preventDefault();
 		var objet = $(this).closest( "div" ).find( "form" );
     	if(objet.length){
     		objet.find( "p" ).hide(); 
@@ -8,9 +13,21 @@ $(document).ready(function() {
     	return false;
     });//end of $('#mainSection div.message').click()
     
-	$("#mainSection div.message form input.cancelEdit").on( 'click', function(){ // le on() devrait pas gérer le live/ajax re-render ?
-        	$(this).parent( "div" ).hide(); 
-        	$(this).parent( "div" ).siblings("p").show(); 
+	$('#mainSection div.message form input.cancelEdit').on( 'click', function(e){ // le on() devrait pas gérer le live/ajax re-render ?
+		e.preventDefault();
+		$(this).parent( "div" ).hide(); 
+        $(this).parent( "div" ).siblings("p").show(); 
     	return false;
     });//end of $('#mainSection div.message').click()
-});//end of $(document).ready(fn)
+}//end of initAll()
+
+/*
+ * Callback pour <f:ajax> : permet de recharger les handlers jQuery après un render Ajax via JSF.
+ * Sans cette fonction, les selecteurs jQuey appliqués aux éléments du DOM qui sont rechargés par
+ * l'appel à <f:ajax> sont perdus et ne fonctionnent plus ensuite.
+ */
+function initAllCallback(e) {
+	if(e.status == "success") {  
+		initAll();
+	}
+}
