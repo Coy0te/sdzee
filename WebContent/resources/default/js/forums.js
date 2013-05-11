b0f88bf5cf2081b442db495f187031d40d7187ba
med <mederic.munier@gmail.com>
2013-05-11 12:51:55 +0800

    (dernière ?) correction de l'édition ajaxisée d'une réponse

--- WebContent/resources/default/js/forums.js
+++ WebContent/resources/default/js/forums.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {	
 	$('#mainSection a.editAction').click(function(){
-    	if($(this).closest( "div" ).find( "form" ).length){
-    		$(this).closest( "div" ).find( "p" ).hide(); 
-        	$(this).closest( "div" ).find( "form" ).show(); 
+		var objet = $(this).closest( "div" ).find( "form" );
+    	if(objet.length){
+    		objet.find( "p" ).hide(); 
+    		objet.find( "div" ).show(); 
     	}
     	return false;
     });//end of $('#mainSection div.message').click()
     
 	$("#mainSection div.message form input.cancelEdit").click(function(){
-        	$(this).parent( "form" ).hide(); 
-        	$(this).parent( "form" ).siblings("p").show(); 
+        	$(this).parent( "div" ).hide(); 
+        	$(this).parent( "div" ).siblings("p").show(); 
     	return false;
     });//end of $('#mainSection div.message').click()
 });//end of $(document).ready(fn)
